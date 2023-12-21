9363c189bd3c2236724011ed0a9eb5fc488c836a
zuzanna.eiti <01150958@pw.edu.pl>
2023-12-21 07:20:56 +0100

    added start_point

--- dynamic-timetable/agents/PassengerAgent.py
+++ dynamic-timetable/agents/PassengerAgent.py
@@ -3,18 +3,24 @@ from spade.behaviour import OneShotBehaviour, CyclicBehaviour
 from spade.message import Message
 from spade.template import Template
 import json
+import random
 
 class PassengerAgent(Agent):
     def __init__(self, jid, password):
         super().__init__(jid, password)
         self.selectDestination()
+        self.bus_id = None
 
     def selectDestination(self):
+        # można też random
         # TODO: dodać sprawdzanie inputów
         print("Destination 1. passenger")
-        x = float(input("x coordinate: "))
-        y = float(input("y coordinate: "))
-        self.destination = [x, y]
+        x_dest = float(input("x coordinate: "))
+        y_dest = float(input("y coordinate: "))
+        self.destination = [x_dest, y_dest]
+        x_start = round(random.random()*50, 2)
+        y_start = round(random.random()*50, 2)
+        self.start_point = [x_start, y_start]
 
     class RequestForTravel(OneShotBehaviour):
         async def run(self):
@@ -22,14 +28,14 @@ class PassengerAgent(Agent):
             msg = Message(to="scheduler@localhost")     # Instantiate the message
             msg.set_metadata("performative", "cfp")  # Set the "inform" FIPA performative
             msg.set_metadata("language", "JSON")        # Set the language of the message content
-            body_dict = {"destination": self.agent.destination}
+            body_dict = {"start_point": self.agent.start_point, "destination": self.agent.destination}
             msg.body = json.dumps(body_dict)                   # Set the message content
 
             await self.send(msg)
             print("Message sent!")
 
             # stop agent from behaviour
-            await self.agent.stop()
+            await self.kill()
 
     async def setup(self):
         print("PassengerAgent started")
