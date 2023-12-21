7e50157d0dc3cb9319490c852df8870a9913cb43
zuzanna.eiti <01150958@pw.edu.pl>
2023-12-21 11:15:41 +0100

    added RoutingBusAgent

--- dynamic-timetable/main.py
+++ dynamic-timetable/main.py
@@ -1,6 +1,7 @@
 import spade
 from agents.PassengerAgent import PassengerAgent
 from agents.SchedulerAgent import SchedulerAgent
+from agents.RoutingBusAgent import RoutingBusAgent
 
 async def main():
     scheduleragent = SchedulerAgent("scheduler@localhost", "scheduler")
@@ -9,9 +10,13 @@ async def main():
     passengeragent = PassengerAgent("passenger@localhost", "passenger")
     await passengeragent.start()
 
+    routingbusagent = RoutingBusAgent("routing_bus@localhost", "routing_bus")
+    await routingbusagent.start()
+
     await spade.wait_until_finished(scheduleragent)
     await scheduleragent.stop()
     await passengeragent.stop()
+    await routingbusagent.stop()
     print("Agents finished")
 
 
