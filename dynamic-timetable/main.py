c2ae9bb3ca5d5ba17b47effe7bf46f88d78bd14b
zuzanna.eiti <01150958@pw.edu.pl>
2023-12-28 17:04:32 +0100

    in path: () -> []

--- dynamic-timetable/main.py
+++ dynamic-timetable/main.py
@@ -4,11 +4,11 @@ from agents.SchedulerAgent import SchedulerAgent
 from agents.RoutingBusAgent import RoutingBusAgent
 
 async def main():
-    initial_path = [(0, 0), (5, 5), (10, 10), (15, 15), (20, 20)]
+    initial_path = [[0, 0], [5, 5], [10, 10], [15, 15], [20, 20]]
     routingbusagent1 = RoutingBusAgent("routing_bus1@localhost", "routing_bus1", initial_path)
     await routingbusagent1.start()
 
-    initial_path = [(20, 0), (15, 5), (10, 10), (5, 15), (0, 20)]
+    initial_path = [[20, 0], [15, 5], [10, 10], [5, 15], [0, 20]]
     routingbusagent2 = RoutingBusAgent("routing_bus2@localhost", "routing_bus2", initial_path)
     await routingbusagent2.start()
 
